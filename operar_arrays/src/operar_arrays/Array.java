e466714efbf2b54d05d6823f0d967b5686f091fc
Said <guinasaid@gmail.com>
2021-02-03 09:30:53 +0100

    Buenos dias

---
+++ operar_arrays/src/operar_arrays/Array.java
@@ -0,0 +1,53 @@
+package operar_arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		
+		int m[]=new int[10];
+		int n[]=new int[10];
+		rellenar_array(m);
+		System.out.println();
+		rellenar_array(n);
+		System.out.println();
+		sumar_array(n);
+		
+		
+	}
+  //Metodo que rellena  un Array
+	public static void rellenar_array(int x[]) {
+		int i=0;
+		for ( i=0;i<x.length;i++) {
+			x[i]=(int)(Math.random()*20+1);
+			System.out.print(x[i]+" ");
+		}
+		
+	}
+	
+	//Metodo que imprime un Array
+	
+	public static void imprimir_array(int x[]) {
+		int i=0;
+		
+		for ( i=0;i<x.length;i++) {
+			
+			System.out.print(x[i]+" ");
+		}
+				
+	}
+	
+
+	//Metodo sumar elementos de un Array   
+	
+	public static void sumar_array(int x[]) {
+		int i=0;
+		int suma=0;
+           for ( i=0;i<x.length;i++) {
+			 suma+= x[i];  
+			
+		}
+                  System.out.print(suma);     
+	}
+	
+}
